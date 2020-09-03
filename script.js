ce11ab678d1cb7edee087c52edca0550cdbacdad
yu1k <48705423+yu1k@users.noreply.github.com>
2020-09-03 11:12:47 +0900

    add : 対象ホストに毎分pingを実行し、エラーが出ていたらSlackに通知してくれる機能通知してくれる機能を実装してくれる機能を実装

--- script.js
+++ script.js
@@ -4,9 +4,27 @@ const https = require("https");
 const cron = require("node-cron");
 
 const SlackChannelID = ""; ///動かす時に入力
+
+const targetAddress = ""; //対象のサーバーのIPアドレス
+const exec = require('child_process').exec; //コマンド実行用
+
 module.exports = (robot) => {
-    
+
+    function serverMonitoring(){
+        cron.schedule("* * * * * *", () => {
+            exec("ping -c 1 targetAddress", (err, stdout , stderr) => {
+                if(err){
+                    robot.messageRoom("bot-test", targetAddress + " が落ちてます");
+                }
+                else{
+                    console.log(stdout);
+                }
+            });
+        });
+    }
+
     function main(){
+        serverMonitoring();
         console.log("Start job.");
     }
     main();
