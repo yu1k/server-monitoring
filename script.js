c1f975ed0e916a228f253dd494b2d92913b4013a
yu1k <48705423+yu1k@users.noreply.github.com>
2020-09-03 10:54:23 +0900

    Update : Hubotで動かすための書き方に変更

--- script.js
+++ script.js
@@ -3,8 +3,11 @@
 const https = require("https");
 const cron = require("node-cron");
 
-function main(){
-    console.log("Start job.");
-}
-
-main();
+const SlackChannelID = ""; ///動かす時に入力
+module.exports = (robot) => {
+    
+    function main(){
+        console.log("Start job.");
+    }
+    main();
+};
