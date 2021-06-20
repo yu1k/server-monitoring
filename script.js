07fda1c400712b7863487a93240e0115415b70f8
yu1k <48705423+yu1k@users.noreply.github.com>
2021-06-20 21:51:33 +0900

    Add : サーバを監視し、Slackに投稿するなどの基本機能を作成

--- script.js
+++ script.js
@@ -1,43 +1,71 @@
 "use strict";
 
-//const ping = require("ping");
+const ping = require("ping");
 const https = require("https");
+const cron = require("node-cron");
 
-// 監視対象サーバのホストを設定
+// 監視対象サーバのホストを設定する。`,` で区切れば複数のホストを指定することが可能
 const targetAddress = [""];
 
-// Slackの投稿メッセージ
-let sendMessageData = JSON.stringify({
-    "username": "サーバ監視くん",
-    "text": "サーバが落ちています。対象のホストを確認してください。",
-    "icon_emoji": ":envelope_with_arrow:"
+// Slackに送信する関数
+function sendSlack(host){
+    // Slackの投稿メッセージ
+    let massageData = JSON.stringify({
+        "username": "サーバ監視くん",
+        "text": "監視対象のサーバが落ちています。ホスト: " + host +  " を確認してください。",
+        "icon_emoji": ":envelope_with_arrow:"
+    });
+
+    // Slack Webhook情報の設定
+    let options = {
+        hostname: "hooks.slack.com",
+        port: 443,
+        path: "", // `/services/AAAAA/BBBBB/ccccc`の形式で設定
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Content-Length": Buffer.byteLength(massageData)
+        }
+    };
+
+    // リクエストを投げる
+    let req = https.request(options, (res) => {
+        if(res.statusCode === 200){
+            console.log("OK: " + res.statusCode);
+        }
+        else{
+            console.log("Status Error: " + res.statusCode);
+        }
+    });
+
+    // エラー
+    req.on("error", (err) => {
+        console.error(err);
+    });
+
+    // データ送信
+    req.write(massageData);
+    req.end();
+    console.log(massageData);
+}
+
+// サーバ監視を実行する関数
+function serverMonitoring(){
+    targetAddress.forEach(function(host){
+        ping.sys.probe(host, function(isAlive){
+            if(!isAlive){
+                sendSlack(host);
+                console.log('host ' + host + ' is dead');
+            }
+            else{
+                console.log('host ' + host + ' is alive');
+            }
+        });
+    });
+}
+
+// 2分毎に `serverMonitoring();` を実行
+cron.schedule("0 */2 * * * *", () => {
+    serverMonitoring();
 });
 
-// Slack Webhook情報の設定
-let options = {
-    hostname: "hooks.slack.com",
-    port: 443,
-    path: "",
-    method: "POST",
-    headers: {
-        "Content-Type": "application/json",
-        "Content-Length": Buffer.byteLength(sendMessageData)
-    }
-};
-// リクエストを投げる
-let req = https.request(options, (res) =>{
-    if(res.statusCode === 200){
-        console.log("OK: " + res.statusCode);
-    }
-    else{
-        console.log("Status Error: " + res.statusCode);
-    }
-});
-
-// エラー
-req.on("error", (err) => {
-    console.error(err);
-});
-
-req.write(sendMessageData);
-req.end();
